ffc5bb72f8af646c57136ce5aae1b2e05bc49b94
xnslx <xnslx89@gmail.com>
2021-04-04 21:45:38 -0500

    dynamic routing

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,18 @@
+import Link from 'next/link';
+
+
 function ClientPage() {
     return(
         <div>
             <h1>The Clients Page</h1>
+            <ul>
+                <li>
+                    <Link href="/clients/max">Max</Link>
+                </li>
+                <li>
+                    <Link href="/clients/manu">Manuel</Link>
+                </li>
+            </ul>
         </div>
     )
 };
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link';
 
 function Home() {
     return(
       <div>
         <h1>The Home Page</h1>
+        <ul>
+          <li>
+            <Link href="/portfolio">Portfolio</Link>
+          </li>
+          <li href="/clients">Client</li>
+        </ul>
       </div>
     )
 }
